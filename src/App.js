6274d19d76c255fceebaa06a794ff1b081b9c1d8
krunalsukhwani <krunal.sukhwani@gmail.com>
2024-09-13 19:23:55 -0400

    Added Task List

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-//arrow function
 const App = () => {
-  //JSX Code
-  //return <h1 title='Professor: Krunal'>Hello Friends, Welcome to COMP229</h1>;
-
-  //React code
-  return React.createElement('h1',{title:'Professor: Krunal'},'Hello Friends, Welcome to COMP229');
+  return (
+    <div className="course-tasks">
+      <h2>Course Task</h2>
+      <ul className="task-list">
+        <li>Create React Application</li>
+        <li>Install Required Softwares</li>
+        <li>Complete the assignment</li>
+      </ul>
+    </div>
+  );
 };
 
 export default App;
