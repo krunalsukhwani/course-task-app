1ba98cb49c6c358c4981d1d37c20804e2b75103b
krunalsukhwani <krunal.sukhwani@gmail.com>
2024-09-20 17:28:26 -0400

    Get data from input box

--- src/Components/NewTask/NewTask.js
+++ src/Components/NewTask/NewTask.js
@@ -3,20 +3,26 @@ import './NewTask.css';
 
 const NewTask = props => {
 
+    let enteredText = '';
+
     const addTaskHandler = event => {
         event.preventDefault();
 
         const newTask = {
             id: Math.random().toString(),
-            text: 'My New Task!'
+            text: enteredText
         };
 
         props.onAddTask(newTask);
     };
 
+    const textChangeHandler = event => {
+        enteredText = event.target.value;
+    };
+
     return (
         <form className="new-task" onSubmit={addTaskHandler}>
-            <input type="text" />
+            <input type="text" onChange={textChangeHandler}/>
             <button type="submit">Add Task</button>
         </form>
     );
