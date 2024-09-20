264f9314e6f209950e4f9261ab0c02bb244c8d56
krunalsukhwani <krunal.sukhwani@gmail.com>
2024-09-20 16:58:35 -0400

    Handling Events

--- src/Components/NewTask/NewTask.js
+++ src/Components/NewTask/NewTask.js
@@ -2,8 +2,20 @@ import React from "react";
 import './NewTask.css';
 
 const NewTask = () => {
+
+    const addTaskHandler = event => {
+        event.preventDefault();
+
+        const newTask = {
+            id: Math.random().toString(),
+            text: 'My New Task!'
+        };
+
+        console.log(newTask);
+    };
+
     return (
-        <form className="new-task">
+        <form className="new-task" onSubmit={addTaskHandler}>
             <input type="text" />
             <button type="submit">Add Task</button>
         </form>
